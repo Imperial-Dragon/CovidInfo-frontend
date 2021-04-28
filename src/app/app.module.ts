de67ff1d2c33f5bb4563c283c28bd31da8eb2016
Imperial-Dragon <adityagunjal1365@gmail.com>
2021-04-29 01:07:42 +0530

    Added bootstrap and material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { HttpClientModule } from '@angular/common/http';
+import { MDBBootstrapModule } from 'ng-uikit-pro-standard';
+import { InputsModule, WavesModule } from 'ng-uikit-pro-standard'
+
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { DataTableComponent } from './components/data-table/data-table.component';
+import { HospitalInfoComponent } from './pages/hospital-info/hospital-info.component';
+import { AdminComponent } from './pages/admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataTableComponent,
+    HospitalInfoComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatSelectModule,
+    FormsModule,
+    MatInputModule,
+    InputsModule,
+    WavesModule,
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
